e3ec7884f45865a612ce2d41ba784fc9668d16bf
Naveen Kamal <naveenkamal.2011@gmail.com>
2023-05-04 17:49:22 +0530

    Day 2

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is your first view created in Django !!!")
+    context_dict={'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
+    #return HttpResponse("This is your first view created in Django !!!")
+    return render(request,'rango/index.html',context=context_dict)
 
 def about(request):
     return HttpResponse('This is about Page....')
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -51,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
-
+TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
+#print("TEMPLATES_DIR=",TEMPLATES_DIR)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
